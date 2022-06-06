5beaea73a5d721cae0e800cdec59d08aff1a1a79
katakanakun <fomachanyade@gmail.com>
2022-06-06 14:19:13 +0900

    MOD: ローカルでの実行確認

--- bin/server.js
+++ bin/server.js
@@ -8,7 +8,7 @@ const http = require('http')
 const wss = new WebSocket.Server({ noServer: true })
 const setupWSConnection = require('./utils.js').setupWSConnection
 
-const host = process.env.HOST || 'localhost'
+const host = process.env.VERCEL_URL || 'localhost'
 const port = process.env.PORT || 1234
 
 const server = http.createServer((request, response) => {
